ba938abe2884a27db8c30025c7b553dc927336d1
amberb <amberbradley22@hotmail.com>
2023-09-29 19:29:19 -0700

    729pm

---
+++ parkglen.js
@@ -0,0 +1,44 @@
+ var images = [...document.querySelectorAll('.image')];
+
+
+
+
+var popup = document.querySelector('.popup');
+const largeImage= document.querySelector ('.large-image');
+const leftArrow= document.querySelector ('.left-arrow');
+const rightArrow= document.querySelector ('.right-arrow');
+const close= document.querySelector ('.close');
+
+let index = 0;
+
+
+images.forEach ((item, i) => {
+  item.addEventListener ('click', () => {
+    updateImage (i);
+    popup.classList.toggle('active');
+  })
+})
+
+const updateImage = (i) => {
+  let path = `PARKGLEN/File ${i+1}.jpeg`;
+  largeImage.src = path;
+  index = i;
+  
+}
+
+leftArrow.addEventListener ('click' , () => {
+  if (index > 0) {
+    updateImage (index - 1);
+  }
+})
+
+rightArrow.addEventListener ('click' , () => {
+  if (index < images.length - 1) {
+    updateImage (index + 1);
+  }
+})
+close.addEventListener ('click' , () => {
+      popup.classList.remove('active');
+})
+
+
